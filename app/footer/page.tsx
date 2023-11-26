2c7d3ec3c780eb343b155711f7fbc8972d8d8d1e
Dimas Rachmadika <dimasrachmadika@gmail.com>
2023-11-26 18:18:56 +0700

    fix : responsive footer

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="mt-10 border-t-[1px] hidden md:block">
+    <div className="mt-10 border-t-[1px]">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
           <div className="sm:col-span-2">
